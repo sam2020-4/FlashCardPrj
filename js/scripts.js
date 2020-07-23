28d317d29b9f8b788dec31e3131bf4c6c4edb1cc
matata samuel <mattasamuel3@gmail.com>
2020-07-23 12:10:04 +0300

    edited js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-// $(document).ready(function(){
-//     $(".content").click(function(){
-//         $(".show").show(300);
-//     });
-
-//     $(".show-list").click(function(){
-//         $("#listed").show(3000);
-//     });
-
-//     $(".close-page").click(function(){
-//         $("#listed").hide(3000);
-//     });
-// });
-
-
 $(document).ready(function(){
     $(".here").click(function(){
         $(".show").show(300);
